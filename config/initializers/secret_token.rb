468b38feaad8d65d5bab24709fc5e9b4543d0bc3
Sethupathi Asokan <sethu@joshsoftware.com>
2013-02-23 10:41:15 +0530

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JoshPresentation::Application.config.secret_token = '906e3f2e20441f231098eae61382bebe4c3c4d9d56d4707ba6b202015fb38186fde7fb68388ab5ae5fc0aa13fc9760ae6a12e7e12d29792e0d151822d2e859dd'
